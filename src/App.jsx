9b483bbe2b282e3a465bd5763e5d0d2326b4d399
Elinton-Souza <esc.vistorias@gmail.com>
2025-11-19 19:44:39 -0300

    7º commit - home finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -16,36 +16,36 @@ function App() {
             alt="logo(desktop)"
             className="hidden md:block logo h-[50px]"
           />
-          <h1 className="text-4xl md:text-6xl font-bold text-center">
-            Crie rotinas, Conquiste o futuro!
-          </h1>
+          <nav>
+            <ul className="hidden md:flex md:flex-row items-start md:items-center justify-center md:gap-24 font-semibold md:text-2xl ">
+              <li className="">
+                <a href="inicio.html">Início</a>
+              </li>
+              <li className="">
+                <a href="nossoapp.html">Nosso aplicativo</a>
+              </li>
+              <li className="">
+                <a href="servicos.html">Serviços</a>
+              </li>
+            </ul>
+          </nav>
           <button className="rounded-xl w-40 md:w-30 border-2 bg-[#ec7c31ba] hover:bg-[#EC7D31] cursor-pointer">
             Junte-se a nós
           </button>
         </div>
         <hr />
-        <nav>
-          <ul className="flex flex-col md:flex-row items-start md:items-center justify-center mt-2 mb-10 md:gap-24 font-semibold md:text-2xl md:mt-8">
-            <li className="">
-              <a href="inicio.html">Início</a>
-            </li>
-            <li className="">
-              <a href="nossoapp.html">Nosso aplicativo</a>
-            </li>
-            <li className="">
-              <a href="feed.html">Feed do blog</a>
-            </li>
-            <li className="">
-              <a href="servicos.html">Serviços</a>
-            </li>
-            <li className="">
-              <a href="mais.html">Mais</a>
-            </li>
-          </ul>
-        </nav>
+        <div className="flex flex-col text-center mt-8 letter-spacing-wide gap-4 mb-8">
+          <h1 className="text-3xl md:text-4xl font-bold text-center">
+            Crie rotinas
+          </h1>
+          <h2 className="text-1xl md:text-3xl font-bold text-center">
+            {" "}
+            Conquiste o futuro!
+          </h2>
+        </div>
       </header>
       <main>
-        <section className="m-5 md:m-15 md:mt-20 flex flex-col md:grid md:grid-cols-2 gap-4">
+        <section className="m-5 md:m-15 md:mt-15 flex flex-col md:grid md:grid-cols-2 gap-4">
           <div className="flex justify-center">
             <img src="/menina.png" alt="" className="w-150 md:h-100" />
           </div>
@@ -54,7 +54,7 @@ function App() {
               Transformando rotinas diárias em uma jornada divertida e
               recompensadora.
             </h3>
-            <button className="md:mt-40 rounded-xl w-40 border-2 bg-[#ec7c31ba] hover:bg-[#EC7D31] cursor-pointer">
+            <button className="md:mt-10 rounded-xl w-40 border-2 bg-[#ec7c31ba] hover:bg-[#EC7D31] cursor-pointer">
               Saiba mais
             </button>
           </div>
@@ -119,15 +119,26 @@ function App() {
             <img src="/mission.png" alt="" className="w-150 md:h-100 mt-2" />
           </div>
         </section>
-        <Rodape />
       </main>
-      <footer className="hidden md:block">
-        <h1>Academia de Hábitos - RESPONSIVO</h1>
-        <h3>Transformando Rotinas com Diversão</h3>
+      <hr />
+      <footer className="hidden md:flex md:flex-col md:items-center md:justify-center text-center mt-10">
+        <div>
+          <h1>Academia de Hábitos</h1>
+          <h3>Transformando Rotinas com Diversão</h3>
+        </div>
+        <p className="text-center font-light text-sm mt-2 mb-2">
+          Direitos Autorais © 2025, Academia de Hábitos
+        </p>
       </footer>
       <footer className="md:hidden ">
-        <h1>Academia de Hábitos</h1>
-        <h3>Transformando Rotinas com Diversão</h3>
+        <hr />
+        <div className="flex flex-col justify-center items-center mt-4">
+          <h1>Academia de Hábitos</h1>
+          <h3 className="">Transformando Rotinas com Diversão</h3>
+        </div>
+        <p className="text-center font-light text-sm mt-2 mb-2">
+          Direitos Autorais © 2025, Academia de Hábitos
+        </p>
       </footer>
     </>
   );
